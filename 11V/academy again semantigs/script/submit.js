292fc69039e9f390647c791d8b6f3ba1f9a9b7d5
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2021-10-08 10:56:08 +0300

    Add files via upload

--- 11V/academy again semantigs/script/submit.js
+++ 11V/academy again semantigs/script/submit.js
@@ -1,5 +1,33 @@
-function closssubmit(){
-    var transition = 
-    // document.getElementById("submit").style.display="none";
-    document.getElementById("submit").style.transition="5s";
+// function closssubmit(){
+//     document.getElementById("submit").style.display="none";
+// }
+function tt(){
+    const myTest=new Test(); 
+    ///myTest.closeAdver();
+    myTest.closeTransitionOpacity();
+   // myTest.closeTransitionTop();
 }
+class Test {
+    // constructor(name, year) {
+    //   this.name = name;
+    //   this.year = year;
+    // }
+    // closeAdver() {
+    //   console.log("Çalıştı");
+      
+    // }
+    closeTransitionOpacity(){
+        var closeTransitionOpacity = document.getElementById("submit");
+        closeTransitionOpacity.classList.add("move")
+        // document.getElementById("submit").style.transition="5s";
+        // document.getElementById("submit").style.opacity="0";
+    }
+    // closeTransitionTop(){
+    //     var closeTransitionTop = document.getElementById("submit");
+    //     closeTransitionTop.classList.add("height")
+    // }
+}
+// function height(){
+//     var closeTransitionTop = document.getElementById("submit");
+//     closeTransitionTop.classList.add("height")
+// }
