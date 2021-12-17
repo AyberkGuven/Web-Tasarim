cf69c66a7b73bea3d45599d2a391e8122f369bbf
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2021-12-17 15:19:48 +0300

    modal

---
+++ 11V/Modal/script/modal.js
@@ -0,0 +1,6 @@
+function openModal(){
+    document.getElementsByClassName("modal")[0].style.display = "block" ;
+}
+function closeModal(){
+    document.getElementsByClassName("modal")[0].style.display = "none" ;
+}
