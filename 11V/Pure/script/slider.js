ae32db3a48143b6b0093c5b5f44cf84228f57d5e
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-01-13 14:01:24 +0300

    Pure

--- 11V/Pure/script/slider.js
+++ 11V/Pure/script/slider.js
@@ -1,20 +1,31 @@
-var i = 0;
-function slider() {
-   switch (i <= 3632) {
+// var l = 0;
+// l += 1208;
+function slider(i) {
+   switch (i) {
        case 1:
-           i+=0;
-           document.getElementById("1").style.transform = "translate3d(-"+i+"px, 0, 0)";
+        //    l+=1208;
+            // l==0;
+            // l==0;
+           document.getElementsByClassName("switch-case")[0].style.transform = "translate3d(0, 0, 0)";
            break;
        case 2:
-           i+=1208;
-           document.getElementById("2").style.transform = "translate3d(-"+i+"px, 0, 0)";
+        //    l==1208;
+           document.getElementsByClassName("switch-case")[0].style.transform = "translate3d(-1208px, 0, 0)";
            break;
        case 3:
-           i+=1208;
-           document.getElementById("3").style.transform = "translate3d(-"+i+"px, 0, 0)";
+        //    l==2416;
+            // l==2416;
+            // l+=2416;
+           document.getElementsByClassName("switch-case")[0].style.transform = "translate3d(-2416px, 0, 0)";
            break;
        default:
            break;
-   } 
+    } 
+//    console.log(l);
    console.log(i);
 }
+// function slider(i){
+//      var slider = "image/img-0"+i+".jpg";
+//      document.getElementById("5").src = slider;
+//      console.log(i);
+// }
